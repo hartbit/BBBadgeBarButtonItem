8001ff29ba5d6a97073544cd04594313245b4a26
David Hart <david@atipik.ch>
2015-05-05 09:18:06 +0200

    Added framework target

---
+++ Example/BBBadgeBarButtonItemFramework/BBBadgeBarButtonItemFramework.h
@@ -0,0 +1,19 @@
+//
+//  BBBadgeBarButtonItemFramework.h
+//  BBBadgeBarButtonItemFramework
+//
+//  Created by David Hart on 05/05/15.
+//  Copyright (c) 2015 aladenise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBBadgeBarButtonItemFramework.
+FOUNDATION_EXPORT double BBBadgeBarButtonItemFrameworkVersionNumber;
+
+//! Project version string for BBBadgeBarButtonItemFramework.
+FOUNDATION_EXPORT const unsigned char BBBadgeBarButtonItemFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBBadgeBarButtonItemFramework/PublicHeader.h>
+
+
